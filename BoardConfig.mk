5416339d6fe891fa12c00b1f0406519b3432ebf0
Alberto97 <albertop2197@gmail.com>
2015-08-25 20:58:28 +0200

    thea: Move props to the common device tree
     * Rename LOCAL_PATH to DEVICE_PATH
     * Use $(DEVICE_PATH) instead of hardcoded path
    
    Change-Id: I2f584d57a51e0bc5cb563cb2e692e6b95e8cc6f4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,18 +16,18 @@
 # inherit from common msm8226-common
 -include device/motorola/msm8226-common/BoardConfigCommon.mk
 
-LOCAL_PATH := device/motorola/thea
+DEVICE_PATH := device/motorola/thea
 
 # Vendor Unification Init
 TARGET_UNIFIED_DEVICE := true
 TARGET_INIT_VENDOR_LIB := libinit_msm
-TARGET_LIBINIT_DEFINES_FILE := device/motorola/thea/init/init_thea.c
+TARGET_LIBINIT_DEFINES_FILE := $(DEVICE_PATH)/init/init_thea.c
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := thea,thea_umtsds,thea_ds,XT1072,XT1078,XT1079
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
+BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
 TARGET_KERNEL_CONFIG := titan_defconfig
 
 # Partitions
@@ -36,8 +36,11 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10526720
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1157627904
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 5207080960
 
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
 # Recovery
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # inherit from the proprietary version
 -include vendor/motorola/thea/BoardConfigVendor.mk
