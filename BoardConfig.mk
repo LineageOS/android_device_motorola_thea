4883c7ca7613e512615f3ab212b17a7603cad921
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-05-10 16:40:10 +0200

    thea: Switch to standalone libinit
    * No need to build the msm one
    
    Change-Id: I361a21d7efdde7a4e6e38c58d94869b27aa38cd1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,9 +19,9 @@
 DEVICE_PATH := device/motorola/thea
 
 # Vendor Unification Init
+TARGET_INIT_VENDOR_LIB := libinit_thea
+TARGET_RECOVERY_DEVICE_MODULES := libinit_thea
 TARGET_UNIFIED_DEVICE := true
-TARGET_INIT_VENDOR_LIB := libinit_msm
-TARGET_LIBINIT_DEFINES_FILE := $(DEVICE_PATH)/init/init_thea.cpp
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := thea,thea_umtsds,thea_ds,XT1072,XT1078,XT1079
