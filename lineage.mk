d5cc6d6f10e4c591f9331ea8feb9ec3da6c61107
LuK1337 <priv.luk@gmail.com>
2017-01-01 23:08:38 +0100

    thea: cm -> lineage
    Change-Id: I0507a4805274528aaa0879c6cd7db3b500382908

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/thea/full_thea.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := thea
-PRODUCT_NAME := cm_thea
+PRODUCT_NAME := lineage_thea
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
